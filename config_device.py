e6141b647d95ac50fac32ef615fefdf17a908799
iotdev8386 <iotdev8386@gmail.com>
2025-05-03 09:57:03 +0700

    Revert "Fix git ignore"
    This reverts commit adebd074e4272a31ac908fc4e23f44a786c53790.

---
+++ config_device.py
@@ -0,0 +1,9 @@
+config = {
+    "HVAC" : {
+        "clientId": "fill_me","userName": "fill_me","password": "fill_me"
+    },
+    "Water": {"clientId": "fill_me","userName": "fill_me","password": "fill_me"},
+    "Energy": {"clientId": "fill_me","userName": "fill_me","password": "fill_me"},
+    "TestDV": {"clientId": "fill_me","userName": "fill_me","password": "fill_me"},
+    "TestDV2": {"clientId": "fill_me","userName": "fill_me","password": "fill_me"}
+}
